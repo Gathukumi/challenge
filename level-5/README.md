d112251f67103cbf0d502d0ae3cbc931442d9198
Gathukumi <gathukumidavid001@gmail.com>
2023-11-02 15:33:22 +0300

    completed

---
+++ level-5/README.md
@@ -0,0 +1,3 @@
+# create a react  app that fetches data from a public api e.g Rapid api
+
+#
--- level-5/src/DataFetching.jsx
+++ level-5/src/DataFetching.jsx
@@ -1,12 +1,15 @@
-
+// DataFetching.js
 import React, { useState, useEffect } from 'react';
 
 const DataFetching = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [newPost, setNewPost] = useState({ title: '', body: '' });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5); // Number of items to display per page
 
-  useEffect(() => {
+  const fetchData = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((data) => {
@@ -17,7 +20,56 @@ const DataFetching = () => {
         setError(error);
         setLoading(false);
       });
+  };
+
+  useEffect(() => {
+    fetchData();
   }, []);
+
+  const handleAddPost = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((response) => response.json())
+      .then(() => {
+        fetchData();
+        setNewPost({ title: '', body: '' });
+      })
+      .catch((error) => {
+        console.error('Error adding the post:', error);
+      });
+  };
+
+  const handleDeletePost = (postId) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchData();
+        } else {
+          throw new Error('Failed to delete post');
+        }
+      })
+      .catch((error) => {
+        console.error('Error deleting the post:', error);
+      });
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
   if (loading) {
     return <div>Loading...</div>;
@@ -28,13 +80,64 @@ const DataFetching = () => {
   }
 
   return (
-    <div>
-      <h2>API Data</h2>
+    <div className="max-w-md mx-auto p-4">
+      <h2 className="text-2xl font-bold mb-4">API Data</h2>
+      <input
+        type="text"
+        className="border border-gray-300 p-2 rounded mb-2"
+        placeholder="Title"
+        value={newPost.title}
+        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+      />
+      <input
+        type="text"
+        className="border border-gray-300 p-2 rounded mb-2"
+        placeholder="Body"
+        value={newPost.body}
+        onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
+      />
+      <button
+        className="custom-button"
+        onClick={handleAddPost}
+      >
+        Add Post
+      </button>
       <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
+        {currentItems.map((item) => (
+          <li key={item.id} className="border border-gray-300 p-2 rounded mb-2">
+            {item.title}
+            <button
+              className="custom-button"
+              onClick={() => handleDeletePost(item.id)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
+      <div className="pagination">
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => paginate(number)}
+            className={number === currentPage ? 'active' : ''}
+          >
+            {number}
+          </button>
+        ))}
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
