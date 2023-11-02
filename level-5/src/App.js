b06e97a678abd2e448ae8c6438b1d03a1d946211
Gathukumi <gathukumidavid001@gmail.com>
2023-11-02 15:13:00 +0300

    completed

---
+++ level-5/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DataFetching from './DataFetching';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React API Data Fetching</h1>
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
---
+++ level-5/src/DataFetching.jsx
@@ -0,0 +1,42 @@
+
+import React, { useState, useEffect } from 'react';
+
+const DataFetching = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h2>API Data</h2>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DataFetching;
